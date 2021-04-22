7eb9dee80099652100b3cd77ae7808923a23108b
Deb Taylor <deb.taylor@intel.com>
2021-04-22 14:47:01 -0400

    Correct table; add version info
    Signed-off-by: Deb Taylor <deb.taylor@intel.com>

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-
+import lpot.version
 
 # -- Project information -----------------------------------------------------
 
@@ -24,7 +24,7 @@ copyright = '2021, Intel® Low Precision Optimization Tool'
 author = 'Intel® LPOT developers'
 
 # The short X.Y version
-version = ''
+version = lpot.version.__version__
 # The full version, including alpha/beta/rc tags
 release = ''
 
